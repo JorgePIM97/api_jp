eeff725af186e79f23898b70bbc41519e72f9a55
jorgesk8 <jorgevillegask8@gmail.com>
2026-08-25 16:37:44 -0600

    autenticacion

--- productos/views.py
+++ productos/views.py
@@ -1,18 +1,30 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Producto, Categoria
 
 from .serializers import (
     ProductoSerializer,
+    ProductoSimpleSerializer,
     CategoriaSimpleSerializer,
     CategoriaDetalleSerializer
 )
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class ProductoViewSet(ModelViewSet):
 
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+    def get_permissions(self):
+
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+
+        return [IsAdminUser()]
 
 
 class CategoriaViewSet(ModelViewSet):
@@ -25,3 +37,20 @@ class CategoriaViewSet(ModelViewSet):
             return CategoriaDetalleSerializer
 
         return CategoriaSimpleSerializer
+
+    @action(
+        detail=True,
+        methods=['get']
+    )
+    def productos(self, request, pk=None):
+
+        categoria = self.get_object()
+
+        productos = categoria.productos.all()
+
+        serializer = ProductoSimpleSerializer(
+            productos,
+            many=True
+        )
+
+        return Response(serializer.data)
